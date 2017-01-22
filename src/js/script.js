1ea8489471f2c4329688b2273ddeaed2de06e836
Karel Van Haeverbeek <karelvanhaeverbeek@gmail.com>
2017-01-22 22:36:54 +0100

    added favicon + title and favicon change on focusloss

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,12 @@
 
   console.log(`hey`);
 
+  const $favicon = document.querySelector(`#favicon`);
+  const title = document.title;
+  const newTitle = `Adventure awaits!`;
+  document.addEventListener(`visibilitychange`, function() {
+    document.title = document.hidden ? newTitle : title;
+    document.hidden ? $favicon.href = `./assets/img/logor.ico` : $favicon.href = `./assets/img/logob.ico`;
+  });
+
 })();
